37c1410668b4a08d7a30b54987e166a1502d1ac4
sey323 <sey323_nm@yahoo.co.jp>
2023-06-08 22:53:28 +0900

    :sparkles: バックエンドから取得した位置情報を画面に表示

--- src/components/map/GoogleMap.vue
+++ src/components/map/GoogleMap.vue
@@ -1,144 +1,117 @@
 <script lang="ts">
-import { Loader } from '@googlemaps/js-api-loader';
+import { Loader } from '@googlemaps/js-api-loader'
+import { defineComponent, ref, computed, onBeforeMount, onMounted } from 'vue'
+import { useStore } from '@/store'
+import { MarkerClusterer } from '@googlemaps/markerclusterer'
+import type { UserReportModel } from '@/types/typescript-axios'
 
-export default {
-  name: 'GoogleMap',
-  
-  data : function(){
+export default defineComponent({
+  data: function () {
     return {
       map: null,
-      google: null,
-      
+      google: null
     }
   },
-  methods : {},
-  mounted : function(){
-    new Loader({
-		apiKey: import.meta.env.VITE_MAP_API,
-		version: 'frozen',
-		libraries: ["places", "drawing", "geometry", "visualization"],
-		language: 'ja',
+  setup() {
+    const store = useStore()
+
+    // バックエンドから申告の情報取得
+    const userReport = computed(() => store.getters['userReports/getAllList'])
+    onBeforeMount(async () => {
+      await store.dispatch('userReports/getAllList')
+      createMapLoader()
     })
-    .load()
-    .then((google) => {
 
+    const googleMapEl = ref()
 
-// The location of ueno
-const ueno = { lat: 35.712223, lng: 139.771118 };
-// The map, centered at ueno
-const map = new google.maps.Map(
-  document.getElementById("map") as HTMLElement,
-  {
-    zoom: 15,
-    center: ueno,
-  }
-);
-const request = {
-    placeId: "ChIJCwbTk56OGGARRJJPe22ziWw",
-    fields: ["name", "formatted_address", "place_id", "geometry"],
-  };
-  const infowindow = new google.maps.InfoWindow();
-  const service = new google.maps.places.PlacesService(map);
-// The marker, positioned at ueno
-const marker = new google.maps.Marker({
-  position: ueno,
-  map: map,
-});
+    // 参考URL: https://qiita.com/shiba_mitue/items/c0795667ca9dd06b9140
+    const loader = new Loader({
+      apiKey: import.meta.env.VITE_MAP_API,
+      version: 'frozen',
+      libraries: ['places', 'drawing', 'geometry', 'visualization'],
+      language: 'ja'
+    })
 
-const beaches = [
-    ["Bondi Beach", 35.712223, 139.771118],
-    ["Bondi Beach1", 35.712223, 139.781119],
-    ["Bondi Beach2", 35.712223, 139.791120],
-    ["Bondi Beach3", 35.712223, 139.761121],
-    ["Bondi Beach4", 35.712223, 139.751122],
-];
-
-// マーカー一つだと詳細表示できるけど、複数の時のやりかたがまだできていない
-function setMarkers(map) {
-        const image = {
-          url: "https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png",
-          size: new google.maps.Size(20, 32),
-          origin: new google.maps.Point(0, 0),
-          anchor: new google.maps.Point(0, 32),
-        };
-        const shape = {
-          coords: [1, 1, 1, 20, 18, 20, 18, 1],
-          type: "poly",
-        };
-
-        for (let i = 0; i < beaches.length; i++) {
-          const beach = beaches[i];
-
-          new google.maps.Marker({
-            position: { lat: beach[1], lng: beach[2] },
-            map,
-            //icon: image,
-            //shape: shape,
-            //title: beach[0],
-            //zIndex: beach[3],
-          });
-        }
-      }
-setMarkers(map);
-service.getDetails(request, (place, status) => {
-    if (
-    status === google.maps.places.PlacesServiceStatus.OK &&
-    place &&
-    place.geometry &&
-    place.geometry.location
-    ) {
-    const marker = new google.maps.Marker({
-        map,
-        position: place.geometry.location,
-    });
-
-    google.maps.event.addListener(marker, "click", () => {
-        const content = document.createElement("div");
-        const nameElement = document.createElement("h2");
-
-        nameElement.textContent = place.name;
-        content.appendChild(nameElement);
-
-        const placeIdElement = document.createElement("p");
-
-        placeIdElement.textContent = place.place_id;
-        content.appendChild(placeIdElement);
-
-        const placeAddressElement = document.createElement("p");
-
-        placeAddressElement.textContent = place.formatted_address;
-        content.appendChild(placeAddressElement);
-        infowindow.setContent(content);
-        infowindow.open(map, marker);
-    });
+    const GOOGLE = ref()
+    const createMapLoader = () => {
+      loader
+        .load()
+        .then((google) => {
+          // 地図の初期化
+          GOOGLE.value = google
+          initMap()
+        })
+        .catch((e) => {
+          console.log(e)
+        })
+        .finally(() => {
+          initMarker()
+        })
     }
-});
-window.map = map;
-}
 
-    )
-    .catch(e => {
-      console.error(e);
-    });
-  },
-}
+    // 地図の初期化
+    const MAP = ref()
+    const initMap = () => {
+      const mapOptions = {
+        center: {
+          lat: 35.6769883,
+          lng: 139.7588499
+        },
+        zoom: 14
+      }
+      MAP.value = new GOOGLE.value.maps.Map(googleMapEl.value, mapOptions)
+    }
+
+    // マーカーに関する設定
+    const markerClusterer = ref()
+    const initMarker = () => {
+      const markers = [] as any[]
+      markerClusterer.value = new MarkerClusterer({ markers, map: MAP.value })
+
+      userReport.value.userReports.forEach((element: UserReportModel) => {
+        // マーカー作成
+        const marker = new GOOGLE.value.maps.Marker({
+          position: { lat: element.location.latitude, lng: element.location.longitude },
+          title: element.content,
+          icon: element.image_url
+        })
+
+        const infoWindow = new GOOGLE.value.maps.InfoWindow()
+        // クリックイベントを追加
+        marker.addListener('click', () => {
+          infoWindow.close()
+
+          const p_node = document.createElement('p')
+
+          const image_node = document.createElement('img')
+          image_node.width = 24
+          image_node.height = 24
+          image_node.src = element.image_url!
+
+          var text_node = document.createTextNode(element.content)
+
+          p_node.appendChild(image_node)
+          p_node.appendChild(text_node)
+
+          infoWindow.setContent('テスト')
+          infoWindow.open(marker.getMap(), marker)
+        })
+        markerClusterer.value.addMarker(marker)
+      })
+    }
+
+    console.log(userReport)
+    return { googleMapEl }
+  }
+})
 </script>
 
 <template>
-    <div id="map"
-      class="pa-0"
-    >
-      google map
-    </div>
-    <!--<iframe 
-        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d810.3326425623934!2d139.70214685694324!3d35.67022196781234!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x0!2zMzXCsDQwJzEyLjgiTiAxMznCsDQyJzA5LjciRQ!5e0!3m2!1sja!2sjp!4v1571820504380!5m2!1sja!2sjp" 
-        width="100%"
-        frameborder="0"
-        style="border:0; height: 100vh;"></iframe>-->
+  <div ref="googleMapEl" class="google-map" />
 </template>
 
 <style>
-#map {
+.google-map {
   height: 100vh;
   width: 100%;
 }
