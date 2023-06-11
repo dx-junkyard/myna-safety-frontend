8eaef5164b7e1aa1f6ea3059b1c961a156674b5d
sey323 <sey323_nm@yahoo.co.jp>
2023-06-11 19:44:23 +0900

    :art: ライトモードの時表示されない不具合

--- src/components/common/UserComment.vue
+++ src/components/common/UserComment.vue
@@ -42,7 +42,7 @@ export default {
         >
       </div>
       <div
-        class="block p-2.5 w-full text-sm text-gray-100 bg-gray-10 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+        class="block p-2.5 w-full text-sm text-dark-100 bg-gray-10 rounded-sm border border-gray-300 focus:ring-blue-500 focus:border-blue-500"
       >
         {{ userReportFeedbackComment.comment }}
       </div>
